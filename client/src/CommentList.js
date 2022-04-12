eea66ba90012ed6c41f94cb3f32ea6f41d161832
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-12 19:16:41 +0530

    Add basic React FE and Node express backend routes

---
+++ client/src/CommentList.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+export const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const renderedComments = comments.map((comment) => (
+    <li key={comment.id}>{comment.content}</li>
+  ));
+
+  return <ul>{renderedComments}</ul>;
+};
